5a46bf3f62b4d9c2e9ad88d46a361e1c1a6c721d
Gary Andelin <gary.l.andelin@boeing.com>
2017-05-11 08:02:42 -0700

    Added map logic

--- js/maps.js
+++ js/maps.js
@@ -1,28 +1,30 @@
+var googleMap;
+var selectedName;
 var locations= 
     [
       {
         name: "Mount Baker",
         latitude: 48.7767,
         longitude: -121.8144,
-        zoom: 0
+        zoom: 16
       },
       {
         name: "Mount Rainier",
         latitude: 46.8523,
         longitude: -121.7603,
-        zoom: 0
+        zoom: 16
       },
       {
         name: "Pikes Place Market",
         latitude: 47.6101,
         longitude: -122.3421,
-        zoom: 0
+        zoom: 16
       },
       {
         name: "Puyallup Fairgrounds",
         latitude: 47.1823,
         longitude: -122.2968,
-        zoom: 0
+        zoom: 16
       },
       {
         name: "Space Needle",
@@ -34,13 +36,16 @@ var locations=
         name: "Washington State Capitol Building",
         latitude: 47.0429,
         longitude: -122.8995,
-        zoom: 0
+        zoom: 16
       }
     ];
 
 /*
- * Initialize Google Map
+ * Initialize Google Map with default setting
  */
+$(document).ready(function() { 
+  updateLocation();
+});
 
 /*
  * Update location based upon point of interest
@@ -50,25 +55,41 @@ function updateLocation() {
   $("#latitude").val(locations[locIx].latitude);
   $("#longitude").val(locations[locIx].longitude);
   $("#zoom").val(locations[locIx].zoom);
+  selectedName = locations[locIx].name;
+}
+
+function getCenter() {
+  var center = {};
+  center.lat = parseFloat($("#latitude").val());
+  center.lng = parseFloat($("#longitude").val());
+  return center;
+}
+
+function getZoom() {
+  return parseInt($("#zoom").val());
 }
 
 /*
  * Display google map of selected point of interest
  */
-var googleMap;
 function displayMap() {
-  googleMap = new google.maps.Map( $('#google-map')[0] );
-  googleMap.setCenter( { lat: 47.6205, lng: -122.3493 } );
-  googleMap.setZoom( 16 );
-/*  var marker = new google.maps.Marker( {
-          position: { lat: 47.6205, lng: -122.3493 }
-      } );
-  marker.setMap( googleMap );
+  var ctr = getCenter();
+  var z = getZoom();
+  googleMap = new google.maps.Map(document.getElementById('mymap'), {
+          center: ctr,
+          zoom: z
+        });
+  var marker = new google.maps.Marker( {
+    position: ctr
+  });
+  
+  marker.setMap(googleMap);
 
   var infoWindow = new google.maps.InfoWindow( {
-          content: 'The Space Needle'
-      } );
-  marker.addListener( 'click', function( ) {
-          infoWindow.open( googleMap, marker );
-      } ); */
+    content: selectedName
+  });
+
+  marker.addListener( 'click', function() {
+    infoWindow.open(googleMap, marker)
+  });
 }
